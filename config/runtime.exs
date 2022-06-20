d1947c5fe02ea002a500a0484a0185d4ae455615
Mayel de Borniol <git@mayel.space>
2022-06-20 14:21:56 +1200

    config

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,3 +5,25 @@ import Config
 if Code.ensure_loaded?(Livebook) do
   Livebook.config_runtime()
 end
+
+## bonfire_livebook
+# if Code.ensure_loaded?(Livebook.Config) do
+#   config :livebook, :root_path, Livebook.Config.root_path!("LIVEBOOK_ROOT_PATH")
+
+#   if password = Livebook.Config.password!("LIVEBOOK_PASSWORD") do
+#     config :livebook, authentication_mode: :password, password: password
+#   else
+#     config :livebook, authentication_mode: :token
+#     config :livebook, token: System.get_env("LIVEBOOK_TOKEN", Livebook.Utils.random_id())
+#   end
+
+#   config :livebook,
+#         :cookie,
+#         Livebook.Config.cookie!("LIVEBOOK_COOKIE") || Livebook.Utils.random_cookie()
+
+#   config :livebook,
+#         :default_runtime,
+#         Livebook.Config.default_runtime!("LIVEBOOK_DEFAULT_RUNTIME") ||
+#           {Livebook.Runtime.Embedded, []}
+
+# end
