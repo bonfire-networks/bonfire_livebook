20bcb13daa4a6a422491269ffde5e84bf3ec638b
Mayel de Borniol <git@mayel.space>
2023-12-30 15:26:51 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/801

--- config/bonfire_livebook.exs
+++ config/bonfire_livebook.exs
@@ -2,8 +2,8 @@ import Config
 
 # NOTE: make sure you also copy the config that needs to go in your app's runtime.exs config
 
-config :bonfire_livebook,
-  disabled: false
+# config :bonfire_livebook,
+#   modularity: :disabled
 
 config :livebook, :base_url_path, "/livebook/"
 
